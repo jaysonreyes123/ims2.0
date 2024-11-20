1c60066b0c2de95893e5a538b9975640df2ff050
jaysonreyes001 <jayson.reyes@microbizone.com>
2024-11-20 12:59:29 +0800

    upload

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,10 @@
 <?php
-
-use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LoginControllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post("login",[LoginController::class,'login']);
-Route::get("test",function(){
-    echo "Test";
-});
-
+Route::post("login",[LoginControllers::class,'login']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('user', [LoginController::class, 'details']);
-    Route::get("logout",[LoginController::class,'logout']);
+    Route::get('user', [LoginControllers::class, 'details']);
+    Route::get("logout",[LoginControllers::class,'logout']);
 });
